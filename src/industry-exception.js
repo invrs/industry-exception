f05c5aba004fdfe659aa1f405dbbe368b3ab779b
Winton Welsh <mail@wintoni.us>
2016-05-29 00:31:37 -0700

    Add exception to instance ignore

--- src/industry-exception.js
+++ src/industry-exception.js
@@ -3,6 +3,13 @@ let pe = new PrettyError()
 
 export let exception = Class =>
   class extends Class {
+    static beforeFactoryOnce() {
+      this.industry({
+        ignore: { instance: [ "exception" ] }
+      })
+      super.beforeFactoryOnce()
+    }
+
     exception({ e }) {
       console.log(pe.render(e))
     }
